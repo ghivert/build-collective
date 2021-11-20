aeec8d87d7421367c0170d8984c2659d4a3c6477
Alessandro RINAUDO <alessandro.rinaudo@gmail.com>
2021-11-20 16:15:27 +0100

    Add signup page

---
+++ src/views/SignUp.vue
@@ -0,0 +1,69 @@
+<template lang="html">
+    <form @submit.prevent="signUp">
+      <card
+        title="Enter your username here"
+        subtitle="Type directly in the input and hit enter. All spaces will be converted to _"
+      >
+        <input
+          type="text"
+          class="input-username"
+          v-model="username"
+          placeholder="Type your username here"
+        />
+      </card>
+    </form>
+    <button type="submit">Submit </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { useStore } from 'vuex'
+import Card from '@/components/Card.vue'
+
+export default defineComponent({
+  name: 'SignUp',
+  components: { Card },
+})
+</script>
+
+<style lang="css" scoped>
+.home {
+  padding: 24px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 500px;
+  margin: auto;
+}
+
+.explanations {
+  padding: 12px;
+}
+
+.button-link {
+  display: inline;
+  appearance: none;
+  border: none;
+  background: none;
+  color: inherit;
+  text-decoration: underline;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+}
+
+.input-username {
+  background: transparent;
+  border: none;
+  padding: 12px;
+  outline: none;
+  width: 100%;
+  color: white;
+  font-family: inherit;
+  font-size: 1.3rem;
+}
+</style>
